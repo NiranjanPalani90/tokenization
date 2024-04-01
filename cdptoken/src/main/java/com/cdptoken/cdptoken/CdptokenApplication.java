9f8b71bd19da6409ccb990b337906a11916e55b6
NiranjanPalani90 <niranjanpalani90@gmail.com>
2024-04-01 11:20:06 +0530

    java project changes
    java project changes

---
+++ cdptoken/src/main/java/com/cdptoken/cdptoken/CdptokenApplication.java
@@ -0,0 +1,13 @@
+package com.cdptoken.cdptoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdptokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdptokenApplication.class, args);
+	}
+
+}
---
+++ cdptoken/src/test/java/com/cdptoken/cdptoken/CdptokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdptoken.cdptoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdptokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
